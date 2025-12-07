38f2324fd31b624c664674bf1785623c56bdb040
AayushD0 <aayushdhama2908@gmail.com>
2025-12-07 20:53:41 +0530

    Change Uvicorn port

--- src/main.py
+++ src/main.py
@@ -84,4 +84,4 @@ async def home(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
